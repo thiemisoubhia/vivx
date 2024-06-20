d9e00fe8f4521a03c6939369f2b62c103e7ed76a
thiemisoubhia <99260862+thiemisoubhia@users.noreply.github.com>
2024-06-20 13:59:28 -0300

    Update ClienteService.java

--- src/main/java/com/github/naomisoubhia/ecommerce/service/ClienteService.java
+++ src/main/java/com/github/naomisoubhia/ecommerce/service/ClienteService.java
@@ -1,24 +1,36 @@
 package com.github.naomisoubhia.ecommerce.service;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import com.github.naomisoubhia.ecommerce.model.Cliente;
 import com.github.naomisoubhia.ecommerce.repository.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
+@Transactional
 public class ClienteService {
 
-	@Autowired
-	private ClienteRepository clienteRepository;
+    @Autowired
+    private ClienteRepository clienteRepository;
 
-	public List<Cliente> list() {
-		return clienteRepository.findAll();
-	}
-	
-	public Cliente save(Cliente cliente) {			
-		return clienteRepository.save(cliente);
-	}
+    public List<Cliente> list() {
+        return clienteRepository.findAll();
+    }
+
+    public Cliente findById(Long id) {
+        Optional<Cliente> clienteOptional = clienteRepository.findById(id);
+        return clienteOptional.orElse(null);
+    }
+
+    public Cliente save(Cliente cliente) {
+        // Aqui você poderia incluir lógicas adicionais antes de salvar, se necessário
+        return clienteRepository.save(cliente);
+    }
+
+    public void delete(Long id) {
+        clienteRepository.deleteById(id);
+    }
 }
