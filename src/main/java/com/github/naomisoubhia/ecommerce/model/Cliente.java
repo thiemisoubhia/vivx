a36694c631e884a3465dff1cb21c234474acfd49
thiemisoubhia <99260862+thiemisoubhia@users.noreply.github.com>
2024-06-20 12:29:13 -0300

    Update Cliente.java

--- src/main/java/com/github/naomisoubhia/ecommerce/model/Cliente.java
+++ src/main/java/com/github/naomisoubhia/ecommerce/model/Cliente.java
@@ -1,43 +1,45 @@
 package com.github.naomisoubhia.ecommerce.model;
 
-import javax.persistence.*;
-import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 
 @Entity
-@Table(name = "cliente")
+@Table(name = "clientes")
 public class Cliente {
 	@Id
-	@Column(name = "cliente_id")
-	private String clienteId;
-
-	private String name;
-	private String email;
-
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long codigo_cliente;
 	
-	// Getters and setters
-
-	public String getClienteId() {
-		return clienteId;
+	private String nome;
+	private Long inscricao_federal;
+	private Long cep;
+	
+	public Long getCodigo_cliente() {
+		return codigo_cliente;
 	}
-
-	public void setClienteId(String clienteId) {
-		this.clienteId = clienteId;
+	public void setCodigo_cliente(Long codigo_cliente) {
+		this.codigo_cliente = codigo_cliente;
 	}
-
-	public String getName() {
-		return name;
+	public String getNome() {
+		return nome;
 	}
-
-	public void setName(String name) {
-		this.name = name;
+	public void setNome(String nome) {
+		this.nome = nome;
 	}
-
-	public String getEmail() {
-		return email;
+	public Long getInscricao_federal() {
+		return inscricao_federal;
 	}
-
-	public void setEmail(String email) {
-		this.email = email;
+	public void setInscricao_federal(Long inscricao_federal) {
+		this.inscricao_federal = inscricao_federal;
 	}
-
+	public Long getCep() {
+		return cep;
+	}
+	public void setCep(Long cep) {
+		this.cep = cep;
+	}
 }
