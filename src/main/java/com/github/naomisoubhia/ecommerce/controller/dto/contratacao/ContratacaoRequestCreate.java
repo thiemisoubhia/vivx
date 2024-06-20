aafcd63723750fb3bead96af58cf665ae357d821
thiemisoubhia <99260862+thiemisoubhia@users.noreply.github.com>
2024-06-20 16:50:52 -0300

    Create ContratacaoRequestCreate.java

---
+++ src/main/java/com/github/naomisoubhia/ecommerce/controller/dto/contratacao/ContratacaoRequestCreate.java
@@ -0,0 +1,21 @@
+package com.github.naomisoubhia.ecommerce.controller.dto.contratacao;
+import java.time.LocalDate;
+
+public class ContratacaoRequestCreate {
+    private Long codigo_cliente;
+    private LocalDate data_contratacao;
+    
+    public Long getCodigo_cliente() {
+        return codigo_cliente;
+    }
+    public void setCodigo_cliente(Long codigo_cliente) {
+        this.codigo_cliente = codigo_cliente;
+    }
+    public LocalDate getData_contratacao() {
+        return data_contratacao;
+    }
+    public void setData_contratacao(LocalDate data_contratacao) {
+        this.data_contratacao = data_contratacao;
+    }
+    
+}
