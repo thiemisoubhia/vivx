77ff434a8ec3280111cfa3de5ea080e7bc2a761b
thiemisoubhia <99260862+thiemisoubhia@users.noreply.github.com>
2024-06-20 12:34:17 -0300

    Update ClienteService.java

--- src/main/java/com/github/naomisoubhia/ecommerce/service/ClienteService.java
+++ src/main/java/com/github/naomisoubhia/ecommerce/service/ClienteService.java
@@ -1,32 +1,24 @@
 package com.github.naomisoubhia.ecommerce.service;
 
-import com.github.naomisoubhia.ecommerce.model.Cliente;
-import com.github.naomisoubhia.ecommerce.repository.ClienteRepository;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-
-import java.util.List;
+import com.github.naomisoubhia.ecommerce.model.Cliente;
+import com.github.naomisoubhia.ecommerce.repository.ClienteRepository;
 
 @Service
 public class ClienteService {
 
-    @Autowired
-    private ClienteRepository clienteRepository;
+	@Autowired
+	private ClienteRepository clienteRepository;
 
-    public List<Cliente> findAll() {
-        return clienteRepository.findAll();
-    }
-
-    public Cliente findById(String id) {
-        return clienteRepository.findById(Long.valueOf(id)).orElseThrow(() -> new RuntimeException("Cliente not found"));
-    }
-
-    public Cliente save(Cliente cliente) {
-        return clienteRepository.save(cliente);
-    }
-
-    public void delete(String id) {
-        clienteRepository.deleteById(Long.valueOf(id));
-    }
+	public List<Cliente> list() {
+		return clienteRepository.findAll();
+	}
+	
+	public Cliente save(Cliente cliente) {			
+		return clienteRepository.save(cliente);
+	}
 }
