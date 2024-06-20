c18bf8f51af24cf101d4b0d7ff095416523e3c27
thiemisoubhia <99260862+thiemisoubhia@users.noreply.github.com>
2024-06-20 13:56:27 -0300

    Update SearchedCliente.java

--- src/main/java/com/github/naomisoubhia/ecommerce/controller/dto/cliente/SearchedCliente.java
+++ src/main/java/com/github/naomisoubhia/ecommerce/controller/dto/cliente/SearchedCliente.java
@@ -3,34 +3,30 @@ package com.github.naomisoubhia.ecommerce.controller.dto.cliente;
 import com.github.naomisoubhia.ecommerce.model.Cliente;
 
 public class SearchedCliente {
-	private Long codigo_cliente;
-	private String nome;
-	
-	
-	public Long getCodigo_cliente() {
-		return codigo_cliente;
-	}
+    private Long codigo_cliente;
+    private String nome;
 
-	public void setCodigo_cliente(Long codigo_cliente) {
-		this.codigo_cliente = codigo_cliente;
-	}
+    // Getters e Setters
+    public Long getCodigo_cliente() {
+        return codigo_cliente;
+    }
 
+    public void setCodigo_cliente(Long codigo_cliente) {
+        this.codigo_cliente = codigo_cliente;
+    }
 
-	public String getNome() {
-		return nome;
-	}
+    public String getNome() {
+        return nome;
+    }
 
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 
-
-	public static SearchedCliente toDto(Cliente cliente) {
-		SearchedCliente dto = new SearchedCliente();
-		dto.setCodigo_cliente(cliente.getCodigo_cliente());
-		dto.setNome(cliente.getNome());		
-		return dto;
-	}
-
-
+    public static SearchedCliente toDto(Cliente cliente) {
+        SearchedCliente dto = new SearchedCliente();
+        dto.setCodigo_cliente(cliente.getCodigo_cliente());
+        dto.setNome(cliente.getNome());
+        return dto;
+    }
 }
