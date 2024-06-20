ae542b47b61fbef6b2b069b11c72a83827da0b6c
thiemisoubhia <99260862+thiemisoubhia@users.noreply.github.com>
2024-06-20 13:55:43 -0300

    Update ClienteRequestCreate.java

--- src/main/java/com/github/naomisoubhia/ecommerce/controller/dto/cliente/ClienteRequestCreate.java
+++ src/main/java/com/github/naomisoubhia/ecommerce/controller/dto/cliente/ClienteRequestCreate.java
@@ -1,30 +1,32 @@
 package com.github.naomisoubhia.ecommerce.controller.dto.cliente;
 
-
 public class ClienteRequestCreate {
-	
-private String nome;
-private Long inscricao_federal;
-private Long cep;
+    private String nome;
+    private Long inscricao_federal;
+    private Long cep;
+    
+    // Getters e Setters
+    public String getNome() {
+        return nome;
+    }
 
-public String getNome() {
-	return nome;
-}
-public void setNome(String nome) {
-	this.nome = nome;
-}
-public Long getInscricao_federal() {
-	return inscricao_federal;
-}
-public void setInscricao_federal(Long inscricao_federal) {
-	this.inscricao_federal = inscricao_federal;
-}
-public Long getCep() {
-	return cep;
-}
-public void setCep(Long cep) {
-	this.cep = cep;
-}
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 
+    public Long getInscricao_federal() {
+        return inscricao_federal;
+    }
 
+    public void setInscricao_federal(Long inscricao_federal) {
+        this.inscricao_federal = inscricao_federal;
+    }
+
+    public Long getCep() {
+        return cep;
+    }
+
+    public void setCep(Long cep) {
+        this.cep = cep;
+    }
 }
