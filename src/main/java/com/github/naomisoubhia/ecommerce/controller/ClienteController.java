a5a254e086aae9c1bbde1ab303ef6295ec846e8e
thiemisoubhia <99260862+thiemisoubhia@users.noreply.github.com>
2024-06-20 12:22:03 -0300

    Update ClienteController.java

--- src/main/java/com/github/naomisoubhia/ecommerce/controller/ClienteController.java
+++ src/main/java/com/github/naomisoubhia/ecommerce/controller/ClienteController.java
@@ -1,47 +1,84 @@
-package com.github.naomisoubhia.ecommerce.controller;
-
-import com.github.naomisoubhia.ecommerce.controller.dto.ClienteDTO;
-import com.github.naomisoubhia.ecommerce.model.Cliente;
-import com.github.naomisoubhia.ecommerce.service.ClienteService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.fiap.checkpoint2.controller.dto.cliente.ClienteRequestCreate;
+import br.com.fiap.checkpoint2.controller.dto.cliente.ClienteRequestUpdate;
+import br.com.fiap.checkpoint2.controller.dto.cliente.SearchedCliente;
+import br.com.fiap.checkpoint2.model.Cliente;
+import br.com.fiap.checkpoint2.repository.ClienteRepository;
+import br.com.fiap.checkpoint2.service.ClienteService;
+
+
+
+
 @RestController
-@RequestMapping("/api/clientes")
+@RequestMapping("/clientes")
 public class ClienteController {
 
-    @Autowired
-    private ClienteService clienteService;
+	@Autowired
+	private ClienteService clienteService;
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
 
-    @GetMapping
-    public List<Cliente> getAllClientes() {
-        return clienteService.findAll();
-    }
+	@GetMapping
+	public List<SearchedCliente> listAll() {
+		List<SearchedCliente> result = 
+				clienteService.list()
+				.stream()
+				.map(SearchedCliente::toDto)
+				.collect(Collectors.toList());
+		return result;
+	}
 
-    @GetMapping("/{id}")
-    public ResponseEntity<Cliente> getClienteById(@PathVariable String id) {
-        try {
-            Cliente cliente = clienteService.findById(id);
-            return ResponseEntity.ok(cliente);
-        } catch (RuntimeException e) {
-            return ResponseEntity.notFound().build();
-        }
-    }
+	@PostMapping	
+	public Cliente create(@RequestBody ClienteRequestCreate dto) {
 
-    @PostMapping
-    public Cliente createCliente(@RequestBody Cliente cliente) {
-        return clienteService.save(cliente);
-    }
-    
-    private ClienteDTO convertToClienteDTO(Cliente cliente) {
-        ClienteDTO dto = new ClienteDTO();
-        dto.setClienteId(cliente.getClienteId());
-        dto.setName(cliente.getName());
-        dto.setEmail(cliente.getEmail());
-        return dto;
-    }
+		
+		Cliente cliente = new Cliente();
+		
+		cliente.setNome(dto.getNome());
+		cliente.setCep(dto.getCep());	
+		cliente.setInscricao_federal(dto.getInscricao_federal());
+		
+		Cliente result = clienteService.save(cliente);
+		return result;
+	}
+
+	@PutMapping
+	public Cliente update(Long codigo_cliente,@RequestBody ClienteRequestUpdate dto) {
+		
+		boolean exists =
+				clienteRepository.existsById(dto.getCodigo_cliente());
+		
+		if (!exists) {
+			throw new RuntimeException("Id não encontrado " 
+							+ dto.getCodigo_cliente());			
+		}
+		Cliente cliente = new Cliente();
+
+		cliente.setCodigo_cliente(dto.getCodigo_cliente());
+		cliente.setNome(dto.getNome());
+		cliente.setCep(dto.getCep());	
+		cliente.setInscricao_federal(dto.getInscricao_federal());
+
+		Cliente result = clienteService.save(cliente);
+		return result;
+	}
+
+	@DeleteMapping(value = "{codigo_cliente}")
+	public void delete(@PathVariable Long codigo_cliente) {
+		clienteRepository.deleteById(codigo_cliente);		
+	}
+
 }
