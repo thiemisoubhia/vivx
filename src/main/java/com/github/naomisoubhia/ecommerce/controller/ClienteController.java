9f8564d28474185ea64a4829a44a0a0da9b828e9
thiemisoubhia <99260862+thiemisoubhia@users.noreply.github.com>
2024-06-20 14:19:49 -0300

    Update ClienteController.java

--- src/main/java/com/github/naomisoubhia/ecommerce/controller/ClienteController.java
+++ src/main/java/com/github/naomisoubhia/ecommerce/controller/ClienteController.java
@@ -1,16 +1,3 @@
-package com.github.naomisoubhia.ecommerce.controller.dto;
-
-import com.github.naomisoubhia.ecommerce.controller.dto.cliente.ClienteRequestCreate;
-import com.github.naomisoubhia.ecommerce.controller.dto.cliente.ClienteRequestUpdate;
-import com.github.naomisoubhia.ecommerce.controller.dto.cliente.SearchedCliente;
-import com.github.naomisoubhia.ecommerce.model.Cliente;
-import com.github.naomisoubhia.ecommerce.service.ClienteService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-import java.util.stream.Collectors;
-
 @RestController
 @RequestMapping("/clientes")
 public class ClienteController {
@@ -40,9 +27,15 @@ public class ClienteController {
         if (cliente == null) {
             throw new RuntimeException("Cliente não encontrado com o código: " + codigo_cliente);
         }
+        
+        // Mantém o ID do cliente
+        // cliente.setCodigo_cliente(codigo_cliente); <-- Removido
+        
+        // Atualiza apenas os outros campos
         cliente.setNome(dto.getNome());
         cliente.setCep(dto.getCep());
         cliente.setInscricao_federal(dto.getInscricao_federal());
+        
         return clienteService.save(cliente);
     }
 
