289fc8aa5a88a9fdb390908bb789156772b5cbbc
thiemisoubhia <99260862+thiemisoubhia@users.noreply.github.com>
2024-06-20 15:34:12 -0300

    Update ProdutoService.java

--- src/main/java/com/github/naomisoubhia/ecommerce/service/ProdutoService.java
+++ src/main/java/com/github/naomisoubhia/ecommerce/service/ProdutoService.java
@@ -1,11 +1,12 @@
 package com.github.naomisoubhia.ecommerce.service;
 
-import com.github.naomisoubhia.ecommerce.model.Produto;
+import com.github.naomisoubhia.ecommerce.model.Produtos;
 import com.github.naomisoubhia.ecommerce.repository.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ProdutoService {
@@ -13,19 +14,20 @@ public class ProdutoService {
     @Autowired
     private ProdutoRepository produtoRepository;
 
-    public List<Produto> findAll() {
+    public List<Produtos> list() {
         return produtoRepository.findAll();
     }
 
-    public Produto findById(String id) {
-        return produtoRepository.findById(Long.valueOf(id)).orElseThrow(() -> new RuntimeException("Produto not found"));
-    }
-
-    public Produto save(Produto produto) {
+    public Produtos save(Produtos produto) {
         return produtoRepository.save(produto);
     }
 
-    public void delete(String id) {
-        produtoRepository.deleteById(Long.valueOf(id));
+    public Produtos findById(Long codigo_produto) {
+        Optional<Produtos> produtoOptional = produtoRepository.findById(codigo_produto);
+        return produtoOptional.orElse(null);
+    }
+
+    public void delete(Long codigo_produto) {
+        produtoRepository.deleteById(codigo_produto);
     }
 }
