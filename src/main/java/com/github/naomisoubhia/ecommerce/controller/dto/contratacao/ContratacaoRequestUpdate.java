d2d18db577e09cc2b8414d343df7ac63f1bec3e3
thiemisoubhia <99260862+thiemisoubhia@users.noreply.github.com>
2024-06-20 20:22:55 -0300

    Update ContratacaoRequestUpdate.java

--- src/main/java/com/github/naomisoubhia/ecommerce/controller/dto/contratacao/ContratacaoRequestUpdate.java
+++ src/main/java/com/github/naomisoubhia/ecommerce/controller/dto/contratacao/ContratacaoRequestUpdate.java
@@ -1,37 +1,34 @@
 package com.github.naomisoubhia.ecommerce.controller.dto.contratacao;
 
-import java.time.LocalDate;
+import java.util.Date;
 
 public class ContratacaoRequestUpdate {
-    private Long numero_contratacao, codigo_cliente, codigo_produto;
-    private LocalDate data_contratacao;
-    
-    public Long getNumero_contratacao() {
-        return numero_contratacao;
-    }
-    public void setNumero_contratacao(Long numero_contratacao) {
-        this.numero_contratacao = numero_contratacao;
-    }
+    private Long codigo_cliente;
+    private Long codigo_produto;
+    private Date data_contratacao;
+
+    // Getters and Setters
     public Long getCodigo_cliente() {
         return codigo_cliente;
     }
+
     public void setCodigo_cliente(Long codigo_cliente) {
         this.codigo_cliente = codigo_cliente;
     }
 
-        public Long getCodigo_produto() {
+    public Long getCodigo_produto() {
         return codigo_produto;
     }
+
     public void setCodigo_produto(Long codigo_produto) {
         this.codigo_produto = codigo_produto;
     }
-    
-    public LocalDate getData_contratacao() {
+
+    public Date getData_contratacao() {
         return data_contratacao;
     }
-    public void setData_contratacao(LocalDate data_contratacao) {
+
+    public void setData_contratacao(Date data_contratacao) {
         this.data_contratacao = data_contratacao;
     }
-    
 }
-
