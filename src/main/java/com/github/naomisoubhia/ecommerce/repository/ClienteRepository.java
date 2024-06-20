6ba473807108507c848cfc38e2cc6b79d438d825
thiemisoubhia <99260862+thiemisoubhia@users.noreply.github.com>
2024-06-20 11:38:56 -0300

    Update ClienteRepository.java

--- src/main/java/com/github/naomisoubhia/ecommerce/repository/ClienteRepository.java
+++ src/main/java/com/github/naomisoubhia/ecommerce/repository/ClienteRepository.java
@@ -1,12 +1,12 @@
 package com.github.naomisoubhia.ecommerce.repository;
 
-import com.github.naomisoubhia.ecommerce.model.Client;
+import com.github.naomisoubhia.ecommerce.model.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
 public interface ClienteRepository
-	extends JpaRepository<Client, Long> {
+	extends JpaRepository<Cliente, Long> {
 
 }
