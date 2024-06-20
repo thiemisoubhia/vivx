359f759443bd75ee7bee2a13c26678a1ca6896f4
thiemisoubhia <99260862+thiemisoubhia@users.noreply.github.com>
2024-06-20 11:39:12 -0300

    Update ProdutoRepository.java

--- src/main/java/com/github/naomisoubhia/ecommerce/repository/ProdutoRepository.java
+++ src/main/java/com/github/naomisoubhia/ecommerce/repository/ProdutoRepository.java
@@ -1,11 +1,11 @@
 package com.github.naomisoubhia.ecommerce.repository;
 
-import com.github.naomisoubhia.ecommerce.model.Product;
+import com.github.naomisoubhia.ecommerce.model.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProdutoRepository
-extends JpaRepository<Product, Long> {
+extends JpaRepository<Produto, Long> {
 
 }
