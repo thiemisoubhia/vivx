3c4be08073edc18f8b69038c3c72e310d3a8b598
thiemisoubhia <99260862+thiemisoubhia@users.noreply.github.com>
2024-06-20 18:05:12 -0300

    Update ContratacaoRepository.java

--- src/main/java/com/github/naomisoubhia/ecommerce/repository/ContratacaoRepository.java
+++ src/main/java/com/github/naomisoubhia/ecommerce/repository/ContratacaoRepository.java
@@ -1,11 +1,15 @@
 package com.github.naomisoubhia.ecommerce.repository;
+
+import com.github.naomisoubhia.ecommerce.model.Contratacao;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.github.naomisoubhia.ecommerce.model.Contratacao;
-
+import java.util.List;
 
 @Repository
-public interface ContratacaoRepository extends JpaRepository<Contratacao, Long>{
+public interface ContratacaoRepository extends JpaRepository<Contratacao, Long> {
 
+    List<Contratacao> findByCodigoCliente(Long codigoCliente);
+
+    List<Contratacao> findByCodigoProduto(Long codigoProduto);
 }
