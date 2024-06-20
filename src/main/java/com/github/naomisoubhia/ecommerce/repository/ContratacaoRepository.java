4247820b9bf2870248c11425fd8b71802e230535
thiemisoubhia <99260862+thiemisoubhia@users.noreply.github.com>
2024-06-20 16:42:25 -0300

    Create ContratacaoRepository.java

---
+++ src/main/java/com/github/naomisoubhia/ecommerce/repository/ContratacaoRepository.java
@@ -0,0 +1,11 @@
+package com.github.naomisoubhia.ecommerce.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.fiap.checkpoint2.model.Contratacao;
+
+
+@Repository
+public interface PedidoRepository extends JpaRepository<Contratacao, Long>{
+
+}
