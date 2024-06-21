468a769f4e02b22ba2ee3d57aa4ac2a33a821a55
thiemisoubhia <99260862+thiemisoubhia@users.noreply.github.com>
2024-06-21 14:10:24 -0300

    Update Cliente.java

--- src/main/java/com/github/naomisoubhia/ecommerce/model/Cliente.java
+++ src/main/java/com/github/naomisoubhia/ecommerce/model/Cliente.java
@@ -1,38 +1,42 @@
 package com.github.naomisoubhia.ecommerce.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-
 @Entity
-@Table(name = "clientes")
+@Table(name = "CLIENTES")
 public class Cliente {
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	private Long codigo_cliente;
-	
-	private String nome;
-	private String email;
-	
-	public Long getCodigo_cliente() {
-		return codigo_cliente;
-	}
-	public void setCodigo_cliente(Long codigo_cliente) {
-		this.codigo_cliente = codigo_cliente;
-	}
-	public String getNome() {
-		return nome;
-	}
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "CODIGO_CLIENTE")
+    private Long codigo_cliente;
+    
+    @Column(name = "NOME")
+    private String nome;
+    
+    @Column(name = "EMAIL")
+    private String email;
+    
+    public Long getCodigo_cliente() {
+        return codigo_cliente;
+    }
+    public void setCodigo_cliente(Long codigo_cliente) {
+        this.codigo_cliente = codigo_cliente;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
