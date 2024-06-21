dfdbfef0a76f5d615f537964185872ee62ec8f68
thiemisoubhia <99260862+thiemisoubhia@users.noreply.github.com>
2024-06-21 14:11:27 -0300

    Update Contratacao.java

--- src/main/java/com/github/naomisoubhia/ecommerce/model/Contratacao.java
+++ src/main/java/com/github/naomisoubhia/ecommerce/model/Contratacao.java
@@ -2,52 +2,63 @@ package com.github.naomisoubhia.ecommerce.model;
 
 import java.time.LocalDate;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "contratacoes")
+@Table(name = "CONTRATACOES")
 public class Contratacao {
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	private Long numero_contratacao;
-	
-	private Long codigo_cliente;
-	private Long codigo_produto;
-	private LocalDate data_contratacao;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "NUMERO_CONTRATACAO")
+    private Long numero_contratacao;
+    
+    @ManyToOne
+    @JoinColumn(name = "CODIGO_CLIENTE", referencedColumnName = "CODIGO_CLIENTE")
+    private Cliente cliente;
+    
+    @ManyToOne
+    @JoinColumn(name = "CODIGO_PRODUTO", referencedColumnName = "CODIGO_PRODUTO")
+    private Produto produto;
+    
+    @Column(name = "DATA_CONTRATACAO")
+    private LocalDate data_contratacao;
 
-	//get e set
-	public Long getNumero_contratacao() {
-		return numero_contratacao;
-	}
+    public Long getNumero_contratacao() {
+        return numero_contratacao;
+    }
 
-	public void setNumero_contratacao(Long numero_contratacao) {
-		this.numero_contratacao = numero_contratacao;
-	}
-	
+    public void setNumero_contratacao(Long numero_contratacao) {
+        this.numero_contratacao = numero_contratacao;
+    }
 
-	public Long getCodigo_cliente() {
-		return codigo_cliente;
-	}
-	public void setCodigo_cliente(Long codigo_cliente) {
-		this.codigo_cliente = codigo_cliente;
-	}
+    public Cliente getCliente() {
+        return cliente;
+    }
 
-	public Long getCodigo_produto() {
-		return codigo_produto;
-	}
-	public void setCodigo_produto(Long codigo_produto) {
-		this.codigo_produto = codigo_produto;
-	}
-	
-	public LocalDate getData_contratacao() {
-		return data_contratacao;
-	}
-	public void setData_contratacao(LocalDate data_contratacao) {
-		this.data_contratacao = data_contratacao;
-	}
-	
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public Produto getProduto() {
+        return produto;
+    }
+
+    public void setProduto(Produto produto) {
+        this.produto = produto;
+    }
+
+    public LocalDate getData_contratacao() {
+        return data_contratacao;
+    }
+
+    public void setData_contratacao(LocalDate data_contratacao) {
+        this.data_contratacao = data_contratacao;
+    }
 }
