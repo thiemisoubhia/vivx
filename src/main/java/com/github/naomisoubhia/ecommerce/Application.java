6cda1e36dde8a524e591e592be1945b3f3ee2305
thiemisoubhia <99260862+thiemisoubhia@users.noreply.github.com>
2024-09-04 11:07:45 -0300

    Update Application.java

--- src/main/java/com/github/naomisoubhia/ecommerce/Application.java
+++ src/main/java/com/github/naomisoubhia/ecommerce/Application.java
@@ -2,13 +2,9 @@ package com.github.naomisoubhia.ecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import com.ulisesbocchio.jasyptspringboot.configuration.EnableEncryptablePropertiesConfiguration;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.context.annotation.Import;
+
 
 @SpringBootApplication
-@ConditionalOnProperty(name = "jasypt.encryptor.bootstrap", havingValue = "true", matchIfMissing = true)
-@Import(EnableEncryptablePropertiesConfiguration.class)
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
