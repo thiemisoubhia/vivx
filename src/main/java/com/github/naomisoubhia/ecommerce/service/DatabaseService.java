1142ced7d3e2bbfd695d01586fdb5615853760d0
thiemisoubhia <99260862+thiemisoubhia@users.noreply.github.com>
2024-09-10 08:56:22 -0300

    Update DatabaseService.java

--- src/main/java/com/github/naomisoubhia/ecommerce/service/DatabaseService.java
+++ src/main/java/com/github/naomisoubhia/ecommerce/service/DatabaseService.java
@@ -1,11 +1,19 @@
+package com.github.naomisoubhia.ecommerce.service;
+
+import org.springframework.retry.annotation.Recover;
+import org.springframework.retry.annotation.Retryable;
+import org.springframework.stereotype.Service;
+
+import java.sql.SQLException;
+
 @Service
 public class DatabaseService {
- 
+
     @Retryable(value = { SQLException.class }, maxAttempts = 3)
     public void fetchData() throws SQLException {
         // Código para acessar o banco de dados principal
     }
- 
+
     @Recover
     public void recover(SQLException e) {
         // Código para acessar o banco de dados replicado
