128d2d031e82126b25e65d4b4c6921192d18c2dd
thiemisoubhia <99260862+thiemisoubhia@users.noreply.github.com>
2024-09-11 08:35:42 -0300

    Update EcommerceApplicationTests.java

--- src/test/java/com/github/naomisoubhia/ecommerce/EcommerceApplicationTests.java
+++ src/test/java/com/github/naomisoubhia/ecommerce/EcommerceApplicationTests.java
@@ -1,11 +1,12 @@
 package com.github.naomisoubhia.ecommerce;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootApplication
-public class EcommerceApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(EcommerceApplication.class, args);
+@SpringBootTest
+public class EcommerceApplicationTests {
+
+    @Test
+    void contextLoads() {
     }
 }
