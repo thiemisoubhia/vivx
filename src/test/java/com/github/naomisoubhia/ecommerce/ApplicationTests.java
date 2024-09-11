425ef45e9e5d42d1076ca401586b3e9a82a7456c
thiemisoubhia <99260862+thiemisoubhia@users.noreply.github.com>
2024-09-11 08:37:04 -0300

    Update and rename EcommerceApplicationTests.java to ApplicationTests.java

--- src/test/java/com/github/naomisoubhia/ecommerce/ApplicationTests.java
+++ src/test/java/com/github/naomisoubhia/ecommerce/ApplicationTests.java
@@ -4,9 +4,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-public class EcommerceApplicationTests {
+public class ApplicationTests {
 
     @Test
     void contextLoads() {
+        // Este teste verifica se o contexto da aplicação é carregado corretamente
     }
 }
