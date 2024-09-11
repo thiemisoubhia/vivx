e44aa466fd050ec2760af9a5192467af3354b711
thiemisoubhia <99260862+thiemisoubhia@users.noreply.github.com>
2024-09-11 09:54:14 -0300

    Create EcommerceApplicationTests.java

---
+++ src/test/java/com/github/naomisoubhia/ecommerce/EcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.naomisoubhia.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
