6ffa2ddbbaa504643ce4fdb3a9bc2967d6aba978
thiemisoubhia <99260862+thiemisoubhia@users.noreply.github.com>
2024-09-17 09:45:54 -0300

    Create DynamoDbConfig.java

---
+++ src/main/java/com/github/naomisoubhia/ecommerce/config/DynamoDbConfig.java
@@ -0,0 +1,19 @@
+package com.github.naomisoubhia.ecommerce.config;
+
+import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DynamoDbConfig {
+
+    @Bean
+    public DynamoDbClient dynamoDbClient() {
+        return DynamoDbClient.builder()
+                .region(Region.US_EAST_1)  // Altere para sua região
+                .credentialsProvider(ProfileCredentialsProvider.create())
+                .build();
+    }
+}
