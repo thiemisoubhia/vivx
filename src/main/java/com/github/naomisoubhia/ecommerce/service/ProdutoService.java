c0ce5b17667911bd288f24c1306924df9c2013fd
thiemisoubhia <99260862+thiemisoubhia@users.noreply.github.com>
2024-09-17 09:49:40 -0300

    Update ProdutoService.java

--- src/main/java/com/github/naomisoubhia/ecommerce/service/ProdutoService.java
+++ src/main/java/com/github/naomisoubhia/ecommerce/service/ProdutoService.java
@@ -4,6 +4,7 @@ import com.github.naomisoubhia.ecommerce.model.Produto;
 import com.github.naomisoubhia.ecommerce.repository.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import software.amazon.awssdk.services.dynamodb.model.DynamoDbException;
 
 import java.util.List;
 import java.util.Optional;
@@ -14,12 +15,26 @@ public class ProdutoService {
     @Autowired
     private ProdutoRepository produtoRepository;
 
+    @Autowired
+    private DynamoDbService dynamoDbService;
+
     public List<Produto> list() {
         return produtoRepository.findAll();
     }
 
     public Produto save(Produto produto) {
-        return produtoRepository.save(produto);
+        // Salva no banco de dados Oracle
+        Produto savedProduto = produtoRepository.save(produto);
+        
+        // Tenta salvar no DynamoDB
+        try {
+            dynamoDbService.saveProdutoToDynamoDB(savedProduto);
+        } catch (DynamoDbException e) {
+            // Lida com exceções do DynamoDB (logar ou tratar conforme necessário)
+            System.err.println("Erro ao salvar no DynamoDB: " + e.getMessage());
+        }
+
+        return savedProduto;
     }
 
     public Produto findById(Long codigo_produto) {
@@ -28,6 +43,15 @@ public class ProdutoService {
     }
 
     public void delete(Long codigo_produto) {
+        // Remove do banco de dados Oracle
         produtoRepository.deleteById(codigo_produto);
+
+        // Tenta remover no DynamoDB
+        try {
+            dynamoDbService.deleteProdutoFromDynamoDB(codigo_produto);
+        } catch (DynamoDbException e) {
+            // Lida com exceções do DynamoDB
+            System.err.println("Erro ao deletar no DynamoDB: " + e.getMessage());
+        }
     }
 }
