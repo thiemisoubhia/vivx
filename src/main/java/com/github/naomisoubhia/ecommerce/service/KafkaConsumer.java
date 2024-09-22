49c0a91fdc9beab7678f031ce63fb4082f2ceb5c
caio favaro de oliveira <91095945+CaioFavaro@users.noreply.github.com>
2024-09-22 12:19:22 -0300

    Create KafkaConsumer.java
    Criação do consumer

---
+++ src/main/java/com/github/naomisoubhia/ecommerce/service/KafkaConsumer.java
@@ -0,0 +1,14 @@
+package com.github.naomisoubhia.ecommerce.service;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaConsumer {
+
+    @KafkaListener(topics = "vivx_topic", groupId = "vivx_group")
+    public void consume(String message) {
+        System.out.println("Mensagem recebida: " + message);
+        // Lógica de processamento da mensagem recebida
+    }
+}
