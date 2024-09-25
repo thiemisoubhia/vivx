cf6ba0a088bed8f83dc67a4492cf03a5e459fd82
thiemisoubhia <99260862+thiemisoubhia@users.noreply.github.com>
2024-09-25 10:06:41 -0300

    Update DynamoDbConfig.java

--- src/main/java/com/github/naomisoubhia/ecommerce/config/DynamoDbConfig.java
+++ src/main/java/com/github/naomisoubhia/ecommerce/config/DynamoDbConfig.java
@@ -1,19 +1,19 @@
-package com.github.naomisoubhia.ecommerce.config;
+// package com.github.naomisoubhia.ecommerce.config;
 
-import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
-import software.amazon.awssdk.regions.Region;
-import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+// import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
+// import software.amazon.awssdk.regions.Region;
+// import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
 
-@Configuration
-public class DynamoDbConfig {
+// @Configuration
+// public class DynamoDbConfig {
 
-    @Bean
-    public DynamoDbClient dynamoDbClient() {
-        return DynamoDbClient.builder()
-                .region(Region.US_EAST_1)  // Altere para sua região
-                .credentialsProvider(ProfileCredentialsProvider.create())
-                .build();
-    }
-}
+//     @Bean
+//     public DynamoDbClient dynamoDbClient() {
+//         return DynamoDbClient.builder()
+//                 .region(Region.US_EAST_1)  // Altere para sua região
+//                 .credentialsProvider(ProfileCredentialsProvider.create())
+//                 .build();
+//     }
+// }
